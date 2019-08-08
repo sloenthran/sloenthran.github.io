47d5e2d17e3439dfe37ed891e5d00d2354f3bc09
Dawid Nogacz <sloenthran@gmail.com>
2019-08-08 18:15:34 +0200

    Update script.js

--- JavaTaskApplication/script.js
+++ JavaTaskApplication/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'hhttps://java-task-application.herokuapp.com/v1/task/';
+  const apiRoot = 'https://java-task-application.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://java-task-application.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
