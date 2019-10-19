35261cf3c29d0a1e0f723727590e1548dcc3bfec
Dawid Nogacz <sloenthran@gmail.com>
2019-10-19 18:05:11 +0200

    Update script.js

--- JavaTaskApplication/script.js
+++ JavaTaskApplication/script.js
@@ -111,7 +111,7 @@ $(document).ready(function() {
     var requestUrl = apiRoot + 'tasks';
 
     $.ajax({
-      url: requestUrl + '/?' + $.param({
+      url: requestUrl + '/' + $.param({
         id: taskId
       }),
       method: 'DELETE',
