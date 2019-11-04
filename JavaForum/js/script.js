03784330f6b399b1a4ca0e3cad29c188778dd89e
Dawid Nogacz <sloenthran@gmail.com>
2019-11-04 17:11:35 +0100

    Add files via upload

--- JavaForum/js/script.js
+++ JavaForum/js/script.js
@@ -12,4 +12,28 @@ $(window).on("load", function() {
         $(".wrapper").removeClass("overlay");
         return false;
     });
+
+    //Login Popup
+    $(".login-box").on("click", function(){
+        $("#login-box").addClass("open");
+        $(".wrapper").addClass("overlay");
+        return false;
+    });
+    $("#close-login-box").on("click", function(){
+        $("#login-box").removeClass("open");
+        $(".wrapper").removeClass("overlay");
+        return false;
+    });
+
+    //Register Popup
+    $(".register-box").on("click", function(){
+        $("#register-box").addClass("open");
+        $(".wrapper").addClass("overlay");
+        return false;
+    });
+    $("#close-register-box").on("click", function(){
+        $("#register-box").removeClass("open");
+        $(".wrapper").removeClass("overlay");
+        return false;
+    });
 });
