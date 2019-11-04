f77cb8923a479407951d7a5d2c3a677061d85f0d
Dawid Nogacz <sloenthran@gmail.com>
2019-11-04 15:57:18 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(window).on("load", function() {
+    "use strict";
+
+    //AskQuestion Popup
+    $(".ask-question").on("click", function(){
+        $("#question-box").addClass("open");
+        $(".wrapper").addClass("overlay");
+        return false;
+    });
+    $("#close-question-box").on("click", function(){
+        $("#question-box").removeClass("open");
+        $(".wrapper").removeClass("overlay");
+        return false;
+    });
+});
